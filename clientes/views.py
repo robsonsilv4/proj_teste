dff6396be240ce5ce951eddb089502fc75ff490f
robsonsilv4 <robsonsilv410@gmail.com>
2018-07-28 16:10:59 -0300

    Preparação para o CRUD

--- clientes/views.py
+++ clientes/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def pessoas_list(request):
+    return render(request, 'pessoa.html')
--- proj_teste/urls.py
+++ proj_teste/urls.py
@@ -14,14 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import hello, articles, fname, fname_template
+from django.urls import path, include
+from .views import hello, articles, fname_template
 from django.conf import settings
 from django.conf.urls.static import static
+from clientes import urls as clientes_urls
 
 urlpatterns = [
     path('hello/', hello),
     path('articles/<int:year>/', articles),
     path('pessoa/<str:nome>/', fname_template),
     path('admin/', admin.site.urls),
+    path('pessoa/', include(clientes_urls))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
